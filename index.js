2dd7cb7fc2f5cf0b0d2e0bb9bf08d867dc4deff0
root <root@jenkins-node.x4rsvt0tmetu5gt52slthmo4qe.rx.internal.cloudapp.net>
2023-01-07 05:38:32 +0000

    changed typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Networknuts" }');
+    res.send('{ "response": "Hello From Network Nuts" }');
 });
 
 app.get('/will', function (req, res) {
